34a4d9a95033d31bca61f115f3f95867052cce4f
Jad-Kreit29 <jkrei015@uottawa.ca>
2025-07-11 16:18:26 -0400

    Main page and outlet setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+
+// Site Pages
+import MainPage from './pages/Mainpage'
 
 const App = () => {
+
+  const router = createBrowserRouter (
+
+    createRoutesFromElements(
+
+      <Route path='/' element={<MainLayout />}>
+
+        <Route index element={<MainPage />} />
+
+      </Route>
+
+
+    )
+
+  )
+
+
   return (
-    <div>App</div>
+
+    <div className='antialiased'>
+
+      <RouterProvider router = { router } />
+
+    </div>
+    
   )
 }
 
