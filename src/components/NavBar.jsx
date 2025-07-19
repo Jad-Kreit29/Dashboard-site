8f901f95236bbc33b5546aa90de4259580dd4dcf
Jad-Kreit29 <jkrei015@uottawa.ca>
2025-07-18 22:50:01 -0400

    Added simple bar chart example

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import Logo2 from '../assets/logo2.svg'
 
 const NavBar = () => {
   return (
-    <nav className='bg-gradient-to-r from-0% from-[#0D1F2D] to-70% to-[#1A3D59] text-white py-4 px-8 shadow-md sticky top-0 z-50'>
+    <nav className='bg-gradient-to-r from-0% from-[#0D1F2D] to-80% to-[#1A3D59] text-white py-4 px-8 shadow-md sticky top-0 z-50'>
 
         <div className='container mx-auto flex justify-between items-center'>
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import UsersChart from '../components/UsersChart'
 
 const MainPage = () => {
+
   return (
 
     <>
 
       <NavBar />
 
-      <div>Mainpage</div>
-      
+      <p className='flex justify-center text-[#546A7B] mt-4'>Track the growth of video games and the total activity of the top 10 games!</p>
+
+      <div className='flex justify-center mt-8'>
+
+        <UsersChart />
+
+      </div>
+
     </>
 
   )
